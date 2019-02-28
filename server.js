3948486dea01cb7747191b353bdc37449002e7cf
WangSoda <wangsuda@icloud.com>
2019-02-28 11:54:32 +0800

    修复了个人信息组件重叠的bug

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path'); //系统路径模块
 const app = express();
 
 const hostName = '127.0.0.1'; //ip
-const port = 8080; //端口
+const port = 80; //端口
 
 app.use(express.static(path.join(__dirname, 'public'))); //指定静态文件目录
 
